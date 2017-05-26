e765bfbe9df6b9ee7a898eb821421c9f83f3a947
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-26 12:12:46 +0200

    set up express server

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const hostname = '127.0.0.1';
-const port = 3000;
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-fs.readFile('index.html', (err, html) => {
-    if (err) {
-        throw err;
-    }
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
-        res.write(html);
-        res.end();
-    });
-    server.listen(port, hostname, () => {
-        console.log('server started on port ' + port);
-    });
+const app = express();
+
+// var logger = function (req, res, next) {
+//     console.log('logging...');
+//     next();
+// }
+
+// app.use(logger);
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.listen(3000, function () {
+    console.log('server started on port 3000');
 });
